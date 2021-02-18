c14572c6fe3422cc7b61bf76f473678abaef0e9b
nekn3x <nekn3x.dev@gmail.com>
2021-02-18 16:49:06 +0900

    setting up web server

---
+++ backend/index.ts
@@ -0,0 +1,11 @@
+import { Application } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "aaa";
+});
+
+console.log("http://localhost:8000");
+
+await app.listen({ port: 8000 });
